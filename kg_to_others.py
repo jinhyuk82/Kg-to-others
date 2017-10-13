d38c3d025839382362783fab84ca25d6b274ea67
jinhyuk <jinhyuk.seo@gmail.com>
2017-10-13 19:10:29 +0400

    Reflect Solution in the Udemy lecture.

--- kg_to_others.py
+++ kg_to_others.py
@@ -11,9 +11,8 @@ def convert_result(): #단위를 바꾸는 함수
     result_pounds.insert(END, pounds)
     result_ounces.insert(END, ounces)
 
-text_kg=Text(window, height=1, width=10)
-text_kg.grid(row=0, column=0)
-text_kg.insert(END, "Kg") #단순히 kg 문자 표시
+text_kg=Label(window, text="Kg", height=1, width=10)
+text_kg.grid(row=0, column=0) #단순히 kg 문자 표시
 
 e1_value=StringVar()
 e1=Entry(window, textvariable=e1_value, width=10)
@@ -22,25 +21,19 @@ e1.grid(row=0, column=1) #숫자를 입력 받을 칸
 b1=Button(window, text="Convert", command=convert_result)
 b1.grid(row=0, column=2) #버튼
 
+text_grams=Label(window, text="Label", height=1, width=10)
+text_grams.grid(row=1, column=0) #단순히 Grams 문자열 표시
 result_grams=Text(window, height=1, width=10)
-result_grams.grid(row=2, column=0)
+result_grams.grid(row=2, column=0) #gram 결과값 표시할 Text
 
+text_pounds=Label(window, text="Pounds", height=1, width=10)
+text_pounds.grid(row=1, column=1) #단순히 Pounds 문자열 표시
 result_pounds=Text(window, height=1, width=10)
-result_pounds.grid(row=2, column=1)
+result_pounds.grid(row=2, column=1) #pound 결과값 표시할 Text
 
+text_ounces=Label(window, text="Ounces", height=1, width=10)
+text_ounces.grid(row=1, column=2) #단순히 Ounces 문자열 표시
 result_ounces=Text(window, height=1, width=10)
-result_ounces.grid(row=2, column=2)
-
-text_grams=Text(window, height=1, width=10)
-text_grams.grid(row=1, column=0)
-text_grams.insert(END, "Grams") #단순히 Grams 문자열 표시
-
-text_pounds=Text(window, height=1, width=10)
-text_pounds.grid(row=1, column=1)
-text_pounds.insert(END, "Pounds") #단순히 Pounds 문자열 표시
-
-text_ounces=Text(window, height=1, width=10)
-text_ounces.grid(row=1, column=2)
-text_ounces.insert(END, "Ounces") #단순히 Ounces 문자열 표시
+result_ounces.grid(row=2, column=2) #ounce 결과값 표시할 Text
 
 window.mainloop()
